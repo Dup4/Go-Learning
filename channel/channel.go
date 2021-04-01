67981a3b1498a1918b7928f1bf2371135e82f29a
Dup4 <lyuzhi.pan@gmail.com>
2021-04-01 16:33:55 +0800

    Add: channel

--- channel/channel.go
+++ channel/channel.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	channel := make(chan string)
+	channel := make(chan string, 2)
 
 	go func() {
 		time.Sleep(time.Duration(rand.Intn(60)) * time.Millisecond)
 		channel <- "hello"
+		channel <- "hello2"
 	}()
 
 	msg := <-channel
-	fmt.Printf(msg)
+	msg2 := <-channel
+	fmt.Printf(msg, msg2)
 }
